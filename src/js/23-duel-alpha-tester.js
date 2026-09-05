b18a16dee7cc6d321365b69be0aa4bcec898bcd4
typeivciv <coreyst456@gmail.com>
2026-09-05 05:17:32 -0500

    Bump Alpha tester diagnostics to 0.16.5

--- src/js/23-duel-alpha-tester.js
+++ src/js/23-duel-alpha-tester.js
@@ -1,5 +1,5 @@
 'use strict';
-const ALPHA_TESTER_VERSION='0.16.4';
+const ALPHA_TESTER_VERSION='0.16.5';
 function alphaTesterEl(id){return document.getElementById(id)}
 function alphaTesterMode(){
   if(typeof matchMode!=='undefined'&&matchMode==='arcade')return'Arcade / Solo';
